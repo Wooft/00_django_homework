cad7096835b2119539175fab458b97d3236ac240
Wooft <wooftyaa@gmail.com>
2022-11-15 04:36:18 +0300

    initial commit

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
-    published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    published_at = models.DateTimeField(verbose_name='Дата публикации', auto_now=True)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +13,10 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Имя тега')
+
+class Scope(models.Model):
+    pass
