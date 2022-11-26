0c6b2980721e37e778ce72b66bf085e3f7ea47f4
Wooft <wooftyaa@gmail.com>
2022-11-26 16:02:34 +0300

    All tasks done

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -5,19 +5,21 @@ from .models import Article, Tag, Scope
 
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
+        count = 0
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
 
             is_main = form.cleaned_data
             print(is_main)
-        raise ValidationError('Тут всегда ошибка')
-
+            if is_main.get('is_main') == True:
+                count += 1
+        if count > 1:
+            raise ValidationError('Основным может быть только один тег, снимите лишние галочки')
+            return super().clean()  # вызываем базовый код переопределяемого метода
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
-        return super().clean()  # вызываем базовый код переопределяемого метода
-
 class ScopeInline(admin.TabularInline):
     model = Scope
     formset = ScopeInlineFormset
