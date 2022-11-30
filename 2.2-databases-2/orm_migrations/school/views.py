fe05eabfed8cab94f7638293dfe26770e41a7f85
Michael Koleshichenko <75163342+Wooft@users.noreply.github.com>
2022-11-30 21:02:48 +0300

    All Tasks Done

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -6,9 +6,7 @@ from .models import Student
 
 def students_list(request):
     template = 'school/students_list.html'
-    object_list = Student.objects.all()
-    teachers = Student.objects.all().prefetch_related('teachers')
-    print(teachers)
+    object_list = Student.objects.all().prefetch_related('teachers')
     context = {
         'object_list': object_list
     }
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -26,6 +26,10 @@ ALLOWED_HOSTS = []
 
 SITE_ID = 1
 
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
 
 # Application definition
 
@@ -36,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
