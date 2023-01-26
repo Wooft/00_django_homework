8714353a901abc0da67691477b2af655ff615fbc
wooft <wooftyaa@gmail.com>
2023-01-26 22:41:07 +0300

    Done first task

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,43 +1,57 @@
+import pprint
+
 from rest_framework import serializers
+
+from logistic.models import Product, Stock, StockProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = ['title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
     positions = ProductPositionSerializer(many=True)
-
     # настройте сериализатор для склада
+    class Meta:
+        model = Stock
+        fields = ['address', 'products', 'positions']
 
     def create(self, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # создаем склад по его параметрам
         stock = super().create(validated_data)
-
-        # здесь вам надо заполнить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
+        for position in positions:
+            new_stockProduct = StockProduct(
+                stock=stock,
+                product=position['product'],
+                quantity=position['quantity'],
+                price=position['price']
+            )
+            new_stockProduct.save()
         return stock
 
     def update(self, instance, validated_data):
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
-
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
-
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
-
+        for position in positions:
+            stockproducttoupdate = StockProduct.objects.get(stock=stock, product=position['product'])
+            stockproducttoupdate.quantity = position['quantity']
+            stockproducttoupdate.price = position['price']
+            stockproducttoupdate.save()
         return stock
